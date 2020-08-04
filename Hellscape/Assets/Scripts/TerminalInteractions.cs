3a8bc7c6393e4050884ba4bc7846c93c35a53f92
Al <theonlypurplecat@gmail.com>
2020-08-04 15:02:39 +1200

    Switched to text

---
+++ Hellscape/Assets/Scripts/TerminalInteractions.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerminalInteractions : MonoBehaviour
+{
+
+
+    private bool inposition = false;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Space) && inposition == true)
+        {
+
+        }
+
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        inposition = true;
+    }
+
+
+
+
+
+
+}
