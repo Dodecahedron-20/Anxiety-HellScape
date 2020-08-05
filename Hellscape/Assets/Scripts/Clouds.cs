d5ece359a003cfaa612e6293a8e44a3262fd3143
Al <theonlypurplecat@gmail.com>
2020-08-05 15:00:26 +1200

    Moving clouds wip

---
+++ Hellscape/Assets/Scripts/Clouds.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clouds : MonoBehaviour
+{
+
+
+    [SerializeField]
+    private float Movespeed;
+    [SerializeField]
+    private Vector2 userDirection = Vector2.left;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(userDirection * Movespeed * Time.deltaTime);
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+
+
+
+
+
+
+
+}
---
+++ Hellscape/Assets/Scripts/SpawnClouds.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnClouds : MonoBehaviour
+{
+    [SerializeField]
+    private int delay;
+
+    [SerializeField]
+    private GameObject CloudsPrefab;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(makeclouds());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+
+    private IEnumerator makeclouds()
+    {
+
+        yield return new WaitForSeconds(delay);
+        Instantiate(CloudsPrefab, new Vector3(15, 0, 0), Quaternion.identity);
+        StartCoroutine(makeclouds());
+
+
+    }
+
+
+
+
+}
