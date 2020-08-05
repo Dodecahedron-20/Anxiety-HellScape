18e03ad7abbfb8da25838ee42454fca0a3b13be4
Sean <seangullery@gmail.com>
2020-08-05 13:45:53 +1200

    Plane Script

---
+++ Hellscape/Assets/Scripts/PlaneScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneScript : MonoBehaviour
+{
+
+    public float speed = 1f;
+    public float rotation = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+      
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Vector3 pos = transform.position;
+            pos.y += speed * Time.deltaTime;
+            transform.Rotate(new Vector3(0, 0, rotation));
+            transform.position = pos;
+
+         if (rotation > 0)
+            {
+               //Loadscene
+            }
+     
+        }
+        
+       
+        
+    }
+}
