9949a16bd1886691492a5f2027475a9b77c5fff8
Sean <seangullery@gmail.com>
2020-08-06 14:01:03 +1200

    Updated Version

---
+++ Hellscape/Assets/Scripts/Blinking.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blinking : MonoBehaviour
+{
+
+    public GameObject objectflash;
+
+    
+
+     void Start()
+    {
+        objectflash.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (objectflash.activeSelf == true)
+            {
+                objectflash.SetActive(false);
+            }
+            else
+            {
+                objectflash.SetActive(true);
+            }
+        }
+    } 
+}
+
+
+   
+
