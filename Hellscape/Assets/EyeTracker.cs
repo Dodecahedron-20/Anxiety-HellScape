54b1d0ba6dd9984a5825823b3534a7009929329f
Sean <seangullery@gmail.com>
2020-08-06 12:59:35 +1200

    Eyes Script working

---
+++ Hellscape/Assets/EyeTracker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeTracker : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Player;
+    private Vector3 baseposition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        baseposition = transform.position; 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get vector from zero to player then normalize then mutiply by radius set position to that
+
+       var direction = (Player.transform.position) - (baseposition);
+       var target =  direction.normalized * 0.16f;
+       transform.position = target + baseposition;
+       
+    }
+}
---
+++ Hellscape/Assets/Flashing.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashing : MonoBehaviour
+{
+    public GameObject objectflash;
+    public static bool flash = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (flash)    
+            objectflash.SetActive (false);
+        else
+       
+        objectflash.SetActive(true);
+        
+
+    }
+}
